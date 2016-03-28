03a24bdd6f7cc565b49c41a893625fb1371d3a98
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-03-27 21:41:20 -0500

    added text file with api key and made data frame for fb to display adjusted close with dates

--- app/vol_app.py
+++ app/vol_app.py
@@ -1,23 +1,38 @@
 import requests, datetime, json
-from pprint import pprint
-
+import pandas as pd
+import numpy as np
+import datetime as dt
+import Quandl
 import matplotlib.pyplot as plt
+from pprint import pprint
+from matplotlib import style
+from statistics import mean
+
 # plt.plot([1,2,3,4])
 
+# api_key = open('quandlapikey.txt','r').read()
+
+# df = Quandl.get("FMAC/HPI_TX", authtoken=api_key)
+
+# print(df.head())
 
 
 # Get Apple's stock prices during November 2012 sorted in ascending order excluding column names.  Include only column 4 (closing price) and collapse the frequency to weekly while returning per cent changes.
 # curl "https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?order=asc&exclude_column_names=true&start_date=2012-11-01&end_date=2012-11-30&column_index=4&collapse=weekly&transformation=rdiff
 
 
-with open('../db_scripts/sp500companiesFolder/FB.json') as data_file:
-	companyJSON = json.load(data_file)
-	data  = companyJSON['dataset']['data']
-	cols = companyJSON['dataset']['column_names']
-	close_price = []
-	for d in reversed(data):
-		close_price += [d[4]]
+df = pd.read_json('../db_scripts/sp500companiesFolder/FB.json')['dataset']
 
-	plt.plot(close_price)
-	plt.ylabel('some numbers')
-	plt.show()
+close_price = []
+dates = []
+for d in reversed(df['data']):
+	dates += [d[0]]
+	close_price += [d[11]]
+
+
+x = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in dates]
+
+plt.plot(x, close_price)
+plt.ylabel('some numbers')
+plt.show()
+
