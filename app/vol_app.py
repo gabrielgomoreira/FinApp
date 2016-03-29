09cb3c08516ada290c43fb05832ea851850be0f9
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-03-28 22:50:16 -0500

    App now calculates annualized historic vol and displays proper window vold graph

--- app/vol_app.py
+++ app/vol_app.py
@@ -17,12 +17,14 @@ from statistics import mean
 # print(df.head())
 
 
-# Get Apple's stock prices during November 2012 sorted in ascending order excluding column names.  Include only column 4 (closing price) and collapse the frequency to weekly while returning per cent changes.
-# curl "https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?order=asc&exclude_column_names=true&start_date=2012-11-01&end_date=2012-11-30&column_index=4&collapse=weekly&transformation=rdiff
+# Get Apple's stock prices during November 2012 sodaily_log_retsed in ascending order excluding column names.  Include only column 4 (closing price) and collapse the frequency to weekly while returning per cent changes.
+# curl "https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?order=asc&exclude_column_names=true&stadaily_log_rets_date=2012-11-01&end_date=2012-11-30&column_index=4&collapse=weekly&transformation=rdiff
 
 
 df = pd.read_json('../db_scripts/sp500companiesFolder/FB.json')['dataset']
-days = 252
+horizon = 252
+hor_sqr = np.sqrt(horizon)
+window = 7
 
 close_price = []
 dates = []
@@ -33,15 +35,22 @@ for d in reversed(df['data']):
 
 x_axis = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in dates]
 
-sp0 = close_price[-days:]
-sp1 = sp0[1:]
-zip_sp = zip(sp1,sp0)
+sp0 = close_price[-(horizon+1):]
+zip_sp = zip(sp0[1:],sp0)
+
+#logarithimic price changes
 log_diff = lambda d2, d1: np.log(d2/d1)
+daily_log_rets = [log_diff(d2,d1) for d2, d1 in zip_sp]
+avg_stdev_daily_vol = np.std(daily_log_rets)
+annualized_vol = hor_sqr * avg_stdev_daily_vol * 100
 
-rt = [np.sqrt(days) * log_diff(d2,d1) for d2, d1 in zip_sp]
-vol = np.sqrt(np.var(rt))
-print(vol)
+vol_list = []
+print(len(daily_log_rets))
+for d in range(window-1, len(daily_log_rets)):
+	vol_list += [np.std(daily_log_rets[(d-(window-1)):d]) * 100 * hor_sqr]
 
+print('Annualized vol: %f' % annualized_vol)
+plt.plot(vol_list)
 plt.show()
 
 # plt.plot(x_axis, close_price)
