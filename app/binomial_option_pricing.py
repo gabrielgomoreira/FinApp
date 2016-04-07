e6560dc7383bf07bb9a499f7c26c48e262546815
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-04-07 17:46:53 -0500

    graph probabilities

--- app/binomial_option_pricing.py
+++ app/binomial_option_pricing.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import pprint
+import matplotlib.pyplot as plt
+import collections
 
 american = True
 call = False
@@ -11,12 +12,9 @@ k_strike = 40
 rate = 0.08
 div=0
 t_time = 1
-n_period = 3
+n_period = 100
 h_period= (t_time/n_period)
 vol = 0.3
-
-
-
 
 
 def get_up_down():
@@ -103,6 +101,39 @@ def get_from_putcall_parity(opt_price, call):
 	else:
 		return opt_price+stock_price-cash
 
+def get_probabilties(price_tree):
+	probabilities = {}
+	final_period = len(price_tree)-1
+	price_list = price_tree[final_period]
+	total = len(price_list)
+
+	for i in range(total):
+		price = price_list[i]
+		probabilities[price] = 1 if price not in probabilities else (probabilities[price]+1)
+
+	return probabilities
+
+def graph_probabilities(probabilities):
+
+	od = collections.OrderedDict(sorted(probabilities.items()))
+	keys = 	list(od.keys())
+	vals = list(od.values())
+
+	max_key = max(probabilities, key=probabilities.get)
+	print(max_key) #55
+
+	width = 1/1.5
+
+	plt.title('Distributed chances')
+	plt.bar(keys,vals, width, color='blue')
+	plt.ylabel('Chance')
+	plt.xlabel('Price')
+	plt.show()
+
+
+
+
+
 
 def setup():
 	up, down = get_up_down()
@@ -115,6 +146,8 @@ def setup():
 	opt2_type = 'Call' if not call else 'Put'
 	final_opt2_price = get_from_putcall_parity(final_opt1_price,call)
 	print('Price of %s at K strike %f is: %f'%(opt2_type, k_strike, final_opt2_price))
+	probabilities = get_probabilties(opt_price_tree)
+	graph_probabilities(probabilities)
 
 
 
