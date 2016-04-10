c6961c32796302ba0111fd1ba06579295d5eb2ea
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-04-10 05:50:05 -0500

    created the convergence graph

--- app/option.py
+++ app/option.py
@@ -126,33 +126,44 @@ class Option:
 		Put-Call Parity: {'binomial_model_price': 68.917431170224233, 'black_scholes_price': 68.617115454168868}
 		Rand Vars and Prob: {'mean': 297.58461444354054, 'prob': 0.44808648636919335, 'stdev': 134.75076860002014, 'var': 18157.76963829617}
 
+		
+		# bs_price = 39.581683
+		# binomial_dict = {1: 50.09687943007085, 2: 36.542582435749878, 3: 43.063509629441164, 4: 38.37619403881363, 5: 41.6097982184776, 6: 38.965803280411421, 7: 40.989017717552905, 8: 39.239476986143458, 9: 40.645594832026291, 10: 39.390892424824294, 11: 40.427776498752642, 12: 39.483763603290079, 13: 40.277361562835239, 14: 39.544690156886908, 15: 40.167275007556938, 16: 39.586571101890705, 17: 40.083223358182472, 18: 39.616344758887799, 19: 40.016951388841427, 20: 39.638038990961434, 21: 39.963359189711561, 22: 39.654133094308378, 23: 39.919126544988281, 24: 39.6662256646202, 25: 39.881999098174994}
+
 		"""
-		# bs_price = binomial_model_price
-		# binomial_dict = {}
-		# for n in range(1,26):
-		# 	print(n)
-		# 	self.get_new_binomial_model_class(self.american, self.call, self.stock_price, self.k_strike, self.t_time, self.vol, self.rate, n, self.div)
-		# 	binomial_price = self.get_binomial_model_price()
-		# 	binomial_dict[n] = binomial_price
+		bs_price = binomial_model_price
+		binomial_dict = {}
+		for n in range(1,26):
+			print(n)
+			self.get_new_binomial_model_class(self.american, self.call, self.stock_price, self.k_strike, self.t_time, self.vol, self.rate, n, self.div)
+			binomial_price = self.get_binomial_model_price()
+			binomial_dict[n] = binomial_price
 
-		# print(binomial_dict)
-		# title = 'Binomial Model Convergence to Black Scholes Price of %.3f for %s with vol %.3f and prob %.4f' % (bs_price, self.ticker, self.vol, rand_vars['prob'])
-
-		bs_price = 39.581683
-		binomial_dict = {1: 50.09687943007085, 2: 36.542582435749878, 3: 43.063509629441164, 4: 38.37619403881363, 5: 41.6097982184776, 6: 38.965803280411421, 7: 40.989017717552905, 8: 39.239476986143458, 9: 40.645594832026291, 10: 39.390892424824294, 11: 40.427776498752642, 12: 39.483763603290079, 13: 40.277361562835239, 14: 39.544690156886908, 15: 40.167275007556938, 16: 39.586571101890705, 17: 40.083223358182472, 18: 39.616344758887799, 19: 40.016951388841427, 20: 39.638038990961434, 21: 39.963359189711561, 22: 39.654133094308378, 23: 39.919126544988281, 24: 39.6662256646202, 25: 39.881999098174994}
+		print(binomial_dict)
+		title = 'Binomial Model Convergence to Black Scholes Price of %.3f for %s with vol %.3f and prob %.4f' % (bs_price, self.ticker, self.vol, rand_vars['prob'])
 
 		od = collections.OrderedDict(sorted(binomial_dict.items()))
 		
 		keys = 	list(od.keys())
 		vals = list(od.values())
+		average = np.mean(vals)
+		average_list2plot = []
+		bs_price_list2plot = []
+		for i in vals:
+			average_list2plot += [average]
+			bs_price_list2plot += [bs_price]
 
 		width = 1
 		title = 'Binomial Model Convergence to Black Scholes Price of %.3f for %s with vol %.3f and prob %.4f' % (bs_price, 'TSLA', 0.433764, 0.44808648636919335)
 
 		plt.title(title)
-		plt.plot(keys,vals, width, color='blue')
-		plt.ylabel('Chances of this being the final outcome')
-		plt.xlabel('Price')
+		plt.plot(keys,vals, label='Price per (n)', linewidth=2.0, color='r')
+		plt.plot(keys,average_list2plot, color='b', label='Avg of BM prices', linestyle='dotted')
+		plt.plot(keys,bs_price_list2plot, color='g', label='Black Scholes Price', linestyle='--')
+		plt.legend(loc='upper right')
+		plt.ylabel('Binomial Model Price')
+		plt.xlabel('Number of (n) binomial periods')
+		plt.grid(True)
 		plt.show()
 
 
