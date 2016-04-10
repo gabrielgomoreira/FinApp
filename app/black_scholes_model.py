10613320c57319e81edd5d985628ae69de1e2743
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-04-09 23:17:02 -0500

    tuning black scholes into class

--- app/black_scholes_model.py
+++ app/black_scholes_model.py
@@ -1,42 +1,44 @@
 #!/usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 import collections
 import scipy.stats as st
 from vol_app import calculate_vol
 
-call = True
 
-stock_price = 150.47
-k_strike = 170
-rate = 0.025
-div=0
-vol = calculate_vol('GOOG')/100
-t_time = 1
+class Black_Scholes:
 
+	def __init__(self, call, stock_price, k_strike, t_time, vol, rate, div):
+		self.call = call
+		self.stock_price = stock_price
+		self.k_strike = k_strike
+		self.t_time = t_time
+		self.vol = vol
+		self.rate = rate
+		self.div = div
 
-def get_d1_d2():
-	period_vol = vol * np.sqrt(t_time)
-	d1 = (np.log(stock_price/k_strike) + (rate-div+ ((vol**2)/2))*t_time)/period_vol
-	d2 = d1 - period_vol
-	return d1,d2
+		self.setup()
 
-def get_normal_cdf(distributions, call):
-	if not call:
-		distributions = [ -d for d in distributions]
-	return [st.norm.cdf(d) for d in distributions]
+		
+	def get_d1_d2(self):
+		period_vol = vol * np.sqrt(t_time)
+		d1 = (np.log(stock_price/k_strike) + (rate-div+ ((vol**2)/2))*t_time)/period_vol
+		d2 = d1 - period_vol
+		return d1,d2
 
-def calculate_option_price(probs, call):
-	first_half = stock_price * np.exp(-div*t_time)*probs[0]
-	second_half = k_strike * np.exp(-rate*t_time)*probs[1]
-	opt_price = first_half - second_half if call else second_half - first_half
-	return opt_price
+	def get_normal_cdf(self, distributions, call):
+		if not call:
+			distributions = [ -d for d in distributions]
+		return [st.norm.cdf(d) for d in distributions]
 
-def setup():
-	distributions = get_d1_d2()
-	probs = get_normal_cdf(distributions,call)
-	opt_price = calculate_option_price(probs, call)
-	print(opt_price)
+	def calculate_option_price(self, probs, call):
+		first_half = stock_price * np.exp(-div*t_time)*probs[0]
+		second_half = k_strike * np.exp(-rate*t_time)*probs[1]
+		opt_price = first_half - second_half if call else second_half - first_half
+		return opt_price
 
-setup()
+	def setup(self):
+		distributions = self.get_d1_d2()
+		probs = self.get_normal_cdf(distributions,call)
+		opt_price = calculate_option_price(probs, call)
+		return opt_price
